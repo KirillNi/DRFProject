268c8b0871b9a6803f8687dc52baad639cc20d0a
KirillNi <knizamutdinov@outlook.com>
2022-04-15 12:01:52 +1000

    Lesson_10

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django_filters',
     'rest_framework.authtoken',
     'drf_yasg',
+    'graphene_django',
 ]
 
 MIDDLEWARE = [
@@ -160,4 +161,9 @@ REST_FRAMEWORK = {
     ],
 
     'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.QueryParameterVersioning',
+}
+
+GRAPHENE = {
+    "SCHEMA": "todo.schema.schema",
+    'ATOMIC_MUTATIONS': True,
 }
